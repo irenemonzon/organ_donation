27e329476518bb3db21a5c8cd795f447896f243a
irenemonzon <irene.monzonm@gmail.com>
2024-09-29 17:53:48 +1000

    fix styles button slider videos

--- src/app/components/InfoOrganDonate.tsx
+++ src/app/components/InfoOrganDonate.tsx
@@ -1,11 +1,12 @@
 "use client";
 
-import React, { ReactNode } from "react";
+import React from "react";
 import Image from "next/image";
 import { FacebookShareButton } from "react-share";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import styles from '../styles/slider.module.scss'
 
 const LazyVideo = ({ videoUrl }: { videoUrl: string }) => {
   return (
@@ -50,6 +51,7 @@ const InfoOrganDonate = () => {
     "https://www.youtube.com/embed/dQw4w9WgXcQ",
     "https://www.youtube.com/embed/dQw4w9WgXcQ",
     "https://www.youtube.com/embed/dQw4w9WgXcQ",
+    "https://www.youtube.com/embed/dQw4w9WgXcQ",
   ];
 
   const settings = {
@@ -58,111 +60,100 @@ const InfoOrganDonate = () => {
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
-    arrows: true,
-    adaptiveHeight: false, // Deshabilitar adaptiveHeight
+    arrows: false,
     appendDots: (dots: React.ReactNode) => (
-      <div className="relative z-10">
-        <ul className="flex justify-center space-x-2 bottom-4">{dots}</ul>
+      <div className="relative z-10 mt-2">
+        <ul className="flex justify-center space-x-2 -m-12">{dots}</ul>
       </div>
     ),
-    customPaging: () => (
-      <div className="dot w-3 h-3 bg-gray-500 rounded-full transition-all duration-300 hover:bg-gray-900"></div>
+    customPaging: (i: number) => (
+      <div className={`${styles.dot} slick-active:${styles.activeDot}`} /> 
     ),
   };
+  
 
   return (
     <>
-    <div className="flex w-full mb-20">
-      <div className="bg-green-600 w-[50%] h-6 " />
-    </div>
-    <div className="grid grid-cols-1 md:grid-cols-3 gap-6 p-4 px-16">
+      <div className="flex w-full mb-20">
+        <div className="bg-green-600 w-[50%] h-6 " />
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-6 p-4 px-16">
       <div className="flex flex-col space-y-8">
         {Array(5)
           .fill(0)
           .map((_, index) => (
-            <div
-              key={`video-slider-left-${index}`}
+             <div
+                key={`video-slider-left-${index}`}
               className="w-full h-60 relative overflow-visible rounded-md bg-skeleton-dark"
             >
-              <Slider {...settings}>
+                <Slider {...settings}>
                 <div className="relative w-full h-60 z-0">
                   <LazyVideo videoUrl={videos[index * 2 + 10]} />
-                  <button className="absolute left-[-40px] top-1/2 transform -translate-y-1/2 z-10">
-                  </button>
-                  <button className="absolute right-[-40px] top-1/2 transform -translate-y-1/2 z-10">
-                  </button>
                 </div>
                 <div className="relative w-full h-60 z-0">
                   <LazyVideo videoUrl={videos[index * 2 + 11]} />
                 </div>
-              </Slider>
-            </div>
+                </Slider>
+              </div>
           ))}
       </div>
 
-      <div className="flex flex-col space-y-8">
-        {photos.map((photoUrl, index) => (
-          <div
-            key={`photo-${index}`}
-            className="relative w-full h-60 overflow-hidden rounded-md bg-skeleton-dark"
-          >
-            <Image
-              src={photoUrl}
-              alt={`Photo ${index}`}
-              width={1920}
-              height={1080}
-              className="object-cover w-full h-full"
-            />
-            <FacebookShareButton
-              url={photoUrl}
-              hashtag="#OrganDonation"
-              className="absolute top-1 right-2 group"
-            >
-              <div className="group  p-2 rounded-full">
-                <Image
-                  src="/share-icon.png"
-                  alt="Share on Facebook"
-                  width={46}
-                  height={46}
-                  className="w-7 h-7"
-                />
-              </div>
-              <div className="absolute top-8 right-0 text-skeleton-light font-bold text-xs px-2 py-1 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-                Share
-              </div>
-            </FacebookShareButton>
-          </div>
-        ))}
-      </div>
-
-  
-      <div className="flex flex-col space-y-8">
-        {Array(5)
-          .fill(0)
-          .map((_, index) => (
+        <div className="flex flex-col space-y-8">
+          {photos.map((photoUrl, index) => (
             <div
-              key={`video-slider-right-${index}`}
-              className="w-full h-60 relative overflow-visible rounded-md bg-skeleton-dark"
+              key={`photo-${index}`}
+              className="relative w-full h-60 overflow-hidden rounded-md bg-skeleton-dark"
             >
-              <Slider {...settings}>
-                <div className="relative w-full rounded-md h-60">
-                  <LazyVideo videoUrl={videos[index * 2 + 10]} />
-                  <button className="absolute left-[-40px] top-1/2 transform -translate-y-1/2 z-10">
-                
-                  </button>
-                  <button className="absolute right-[-40px] top-1/2 transform -translate-y-1/2 z-10">
-                
-                  </button>
+              <Image
+                src={photoUrl}
+                alt={`Photo ${index}`}
+                width={1920}
+                height={1080}
+                className="object-cover w-full h-full"
+              />
+              <FacebookShareButton
+                url={photoUrl}
+                hashtag="#OrganDonation"
+                className="absolute top-1 right-2 group"
+              >
+                <div className="group p-2 rounded-full">
+                  <Image
+                    src="/share-icon.png"
+                    alt="Share on Facebook"
+                    width={46}
+                    height={46}
+                    className="w-7 h-7"
+                  />
                 </div>
-                <div className="relative w-full rounded-md h-60">
-                  <LazyVideo videoUrl={videos[index * 2 + 11]} />
+                <div className="absolute top-8 right-0 text-skeleton-light font-bold text-xs px-2 py-1 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                  Share
                 </div>
-              </Slider>
+              </FacebookShareButton>
             </div>
           ))}
+        </div>
+
+        <div className="flex flex-col space-y-8">
+          {Array(5)
+            .fill(0)
+            .map((_, index) => (
+              <div
+                key={`video-slider-left-${index}`}
+                className="w-full h-60 relative overflow-visible rounded-md bg-skeleton-dark"
+              >
+                <Slider {...settings}>
+                  <div className="relative w-full h-60 z-0">
+                    <LazyVideo videoUrl={videos[index * 2 + 10]} />
+                  </div>
+                  <div className="relative w-full h-60 z-0">
+                    <LazyVideo videoUrl={videos[index * 2 + 11]} />
+                  </div>
+                </Slider>
+              </div>
+            ))}
       </div>
-    </div>
-    <div className="flex w-full mt-20 justify-end">
+      </div>
+      <div className="flex w-full mt-20 justify-end">
         <div className="flex bg-green-600 w-[50%] h-6 " />
       </div>
     </>
